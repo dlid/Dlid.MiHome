629ccfab90eca2c47c5cb8c1afaaae32f6b3876e
David Lidström <david.lidstrom@sandvik.com>
2019-11-29 15:07:16 +0100

    Fixed bug with retry delay. Updated example ui

--- Dlid.MiHome.ExampleUI/Form1.Designer.cs
+++ Dlid.MiHome.ExampleUI/Form1.Designer.cs
@@ -37,93 +37,109 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(43, 40);
+            this.label1.Location = new System.Drawing.Point(10, 12);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(124, 32);
+            this.label1.Size = new System.Drawing.Size(62, 15);
             this.label1.TabIndex = 0;
             this.label1.Text = "IP Address";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(89, 92);
+            this.label2.Location = new System.Drawing.Point(10, 48);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(78, 32);
+            this.label2.Size = new System.Drawing.Size(39, 15);
             this.label2.TabIndex = 0;
             this.label2.Text = "Token";
             // 
             // textBoxIpAddress
             // 
-            this.textBoxIpAddress.Location = new System.Drawing.Point(201, 40);
+            this.textBoxIpAddress.Location = new System.Drawing.Point(78, 9);
             this.textBoxIpAddress.Name = "textBoxIpAddress";
-            this.textBoxIpAddress.Size = new System.Drawing.Size(482, 39);
+            this.textBoxIpAddress.Size = new System.Drawing.Size(482, 23);
             this.textBoxIpAddress.TabIndex = 1;
             this.textBoxIpAddress.TextChanged += new System.EventHandler(this.textBoxIpAddress_TextChanged);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(201, 92);
+            this.textBox2.Location = new System.Drawing.Point(78, 45);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(482, 39);
+            this.textBox2.Size = new System.Drawing.Size(482, 23);
             this.textBox2.TabIndex = 1;
             // 
             // textBox3
             // 
             this.textBox3.Font = new System.Drawing.Font("Consolas", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.textBox3.Location = new System.Drawing.Point(43, 285);
+            this.textBox3.Location = new System.Drawing.Point(10, 148);
             this.textBox3.Multiline = true;
             this.textBox3.Name = "textBox3";
             this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.textBox3.Size = new System.Drawing.Size(639, 445);
+            this.textBox3.Size = new System.Drawing.Size(569, 248);
             this.textBox3.TabIndex = 2;
             this.textBox3.Text = "{\r\n \"method\": \"app_start\",\r\n \"params\": []\r\n}";
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(711, 285);
+            this.textBox4.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.textBox4.Location = new System.Drawing.Point(666, 119);
             this.textBox4.Multiline = true;
             this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
             this.textBox4.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.textBox4.Size = new System.Drawing.Size(639, 445);
+            this.textBox4.Size = new System.Drawing.Size(651, 277);
             this.textBox4.TabIndex = 2;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(43, 194);
+            this.label3.Location = new System.Drawing.Point(10, 91);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(104, 32);
+            this.label3.Size = new System.Drawing.Size(52, 15);
             this.label3.TabIndex = 0;
             this.label3.Text = "Request:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(711, 194);
+            this.label4.Location = new System.Drawing.Point(666, 91);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(120, 32);
+            this.label4.Size = new System.Drawing.Size(60, 15);
             this.label4.TabIndex = 0;
             this.label4.Text = "Response:";
             // 
             // comboBox1
             // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(43, 233);
+            this.comboBox1.Location = new System.Drawing.Point(10, 119);
             this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(640, 40);
+            this.comboBox1.Size = new System.Drawing.Size(569, 23);
             this.comboBox1.TabIndex = 3;
-            this.comboBox1.Text = "app_start";
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(585, 187);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "SEND";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 32F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1417, 768);
+            this.ClientSize = new System.Drawing.Size(1337, 409);
             this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBoxIpAddress);
             this.Controls.Add(this.label1);
@@ -132,8 +148,10 @@
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label4);
+            this.Margin = new System.Windows.Forms.Padding(2, 1, 2, 1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
 
         }
 
@@ -148,6 +166,7 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Dlid.MiHome.ExampleUI/Form1.cs
+++ Dlid.MiHome.ExampleUI/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,14 +13,99 @@ namespace Dlid.MiHome.ExampleUI
 {
     public partial class Form1 : Form
     {
+
+        BackgroundWorker bgWorker;
+
+        public Dictionary<string, string> templates = new Dictionary<string, string>
+        {
+            { "General - miIO.info", JsonConvert.SerializeObject(new { method = "miIO.info" }, Formatting.Indented) },
+            { "Vacuum - app_start", JsonConvert.SerializeObject(new { method = "app_start" }, Formatting.Indented) },
+            { "Vacuum - app_stop", JsonConvert.SerializeObject(new { method = "app_stop" }, Formatting.Indented) },
+            { "Vacuum - app_charge", JsonConvert.SerializeObject(new { method = "app_charge" }, Formatting.Indented) },
+            { "Vacuum - get_consumable", JsonConvert.SerializeObject(new { method = "get_consumable" }, Formatting.Indented) },
+            { "Vacuum - get_clean_summary", JsonConvert.SerializeObject(new { method = "get_clean_summary" }, Formatting.Indented) },
+            { "Vacuum - get_status", JsonConvert.SerializeObject(new { method = "get_status" }, Formatting.Indented) },
+            { "Vacuum - get_timer", JsonConvert.SerializeObject(new { method = "get_timer" }, Formatting.Indented) },
+            { "Vacuum - app_zoned_clean", JsonConvert.SerializeObject(new { method = "app_zoned_clean", @params = new int[] {11, 33, 44, 55, 1 }, _comment = "[x1,y1,x2,y2,times]" }, Formatting.Indented) },
+        };
+
         public Form1()
         {
             InitializeComponent();
+
+            bgWorker = new BackgroundWorker();
+            bgWorker.DoWork += BgWorker_DoWork;
+            bgWorker.RunWorkerCompleted += BgWorker_RunWorkerCompleted;
+
+            comboBox1.Items.Clear();
+            templates.Keys.ToList().ForEach(text =>
+            {
+                comboBox1.Items.Add(text);
+            });
+        }
+
+        private void BgWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Result != null && e.Result is Exception)
+            {
+                var msg = (e.Result as Exception).Message;
+                textBox4.Text = "ERROR: " + msg;
+            } else {
+                textBox4.Text = e.Result as string;
+            }
+            button1.Enabled = true;
+            textBox3.Enabled = true;
+            textBox2.Enabled = true;
+            comboBox1.Enabled = true;
+            textBoxIpAddress.Enabled = true;
+            Cursor = Cursors.Default;
+
+        }
+
+        private void BgWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            try
+            {
+                var device = new MiDevice(textBoxIpAddress.Text, textBox2.Text);
+                var response = device.Send(JsonConvert.DeserializeObject(textBox3.Text));
+                e.Result = JsonConvert.SerializeObject(JsonConvert.DeserializeObject(response.ResponseText), Formatting.Indented);
+            } catch (Exception ex)
+            {
+                e.Result = ex;
+            }
         }
 
         private void textBoxIpAddress_TextChanged(object sender, EventArgs e)
         {
 
         }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var cbx = sender as ComboBox;
+            if (cbx.SelectedIndex != -1)
+            {
+                var template = templates[cbx.SelectedItem.ToString()];
+                textBox3.Text = template;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            textBox3.Enabled = false;
+            textBox2.Enabled = false;
+            comboBox1.Enabled = false;
+            textBoxIpAddress.Enabled = false;
+            Cursor = Cursors.WaitCursor;
+            bgWorker.RunWorkerAsync(JsonConvert.DeserializeObject(textBox3.Text));
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
+
+
 }
